005b9ca326bea0e33a7b791f1f8901e262d065bb
DREDD <mrmagnusrus@gmail.com>
2023-04-26 15:45:56 +0300

    create SPA vue component

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="app">
+        <h1>Feedback app</h1>
+        <p>
+            <router-link to="/create" class="mr-2">New feedback</router-link>
+            <router-link to="/about">Go to About</router-link>
+        </p>
+        <!-- route outlet -->
+        <!-- component matched by the route will render here -->
+        <router-view></router-view>
+    </div>
+</template>
