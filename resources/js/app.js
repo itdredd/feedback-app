acd78ec16c0fd8ae1a4ef7598054b4331a3f4076
DREDD <mrmagnusrus@gmail.com>
2023-04-26 15:43:54 +0300

    create vuex store and initialize in app.js vuex, vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,21 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import store from '@/Store/store';
+import {createRouter, createWebHashHistory} from 'vue-router'
+import Create from "@/Pages/Feedback/Create.vue";
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+const routes = [
+    { path: '/create', component: Create },
+]
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +28,8 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(store)
+            .use(router)
             .mount(el);
     },
     progress: {
